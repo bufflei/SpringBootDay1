35c6d41ae7086b05abd527bd0f801c2c615a942e
dl <ding_dl629@163.com>
2019-11-06 20:56:55 +0800

    本地做的修改

--- src/main/java/com/baizhi/action/HelloAction.java
+++ src/main/java/com/baizhi/action/HelloAction.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/hello")
 public class HelloAction {
+	
+	//这是在本地仓库上修改的
     
     //这是在远程仓库上进行的远程修改
     @RequestMapping("/boot")
